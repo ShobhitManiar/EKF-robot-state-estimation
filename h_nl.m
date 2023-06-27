3c3f32bab7bf23db7034d7da42d8e3a6412def64
Shobhit Maniar <135530208+ShobhitManiar@users.noreply.github.com>
2023-06-27 12:18:52 +0100

    Delete h_nl.m

--- h_nl.m
+++
@@ -1,8 +0,0 @@
-function cmat=h_nl(x,l)
-
-for i=1:1:length(l)
-h(:,:,i)=[sqrt((l(i,1)-x(1))^2+(l(i,2)-x(2))^2);
-           wrapToPi(atan2(l(i,2)-x(2),l(i,1)-x(1)))-x(3)];
-end
-
-cmat=[h(:,:,1);h(:,:,2);h(:,:,3);h(:,:,4);h(:,:,5);h(:,:,6)];
